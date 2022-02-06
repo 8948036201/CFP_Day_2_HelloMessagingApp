58ba8103e59237217d40f4bbc59749c65c5bf61b
8948036201 <azharuddin.ansari108@gmail.com>
2022-02-06 23:06:56 +0530

    Added:  Making a REST Call To show Azhar from Bridgelabz

--- src/main/java/com/bridgelabz/hellomessagingapp/controller/RestApiController.java
+++ src/main/java/com/bridgelabz/hellomessagingapp/controller/RestApiController.java
@@ -1,13 +1,22 @@
 package com.bridgelabz.hellomessagingapp.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(("/hello"))
 public class RestApiController {
+    //UC1
     @RequestMapping(value = { "", "/", "/home" })
     public String sayHello() {
         return "Hello from BridgeLabs...!";
+    }
+    //UC-2
+    //using-http://localhost:8080/hello/query?name= Azhar
+    @RequestMapping(value = {"/query"},method = RequestMethod.GET)
+    public String sayHello(@RequestParam(value = "name")String name){
+        return "Hello"+ name + "from BridgeLabs.";
     }
 }
